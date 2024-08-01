c12bbe3962879957716bc0f24be08f3de47ec3dd
jc2026 <james.cheung2@students.mq.edu.au>
2024-08-01 11:04:02 +1000

    Week 1 Changes
    Week 1 Practical

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("~~~~~");
         System.out.println("Red vs. Blue");
-        System.out.println("~~~~~");
     }
 }
-
-
-
